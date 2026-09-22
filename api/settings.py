35c7dad7cc9d437d217589e3eaad0590d0c5ad27
Ve1dra <abdulrasheedyahya866@gmail.com>
2026-09-23 00:05:52 +0100

    Uncommented the last part of settings.py

--- api/settings.py
+++ api/settings.py
@@ -234,13 +234,13 @@ LOGGING = {
 
 logging.config.dictConfig(LOGGING)
 
-# EMAI_FROM_USER = config('Email_FROM_USER')
-# EMAIL_TO_USER = config('EMAIL_TO_USER')
-# SERVER_EMAIL = config('EMAIL_HOST_USER')
+EMAI_FROM_USER = config('EMAIL_FROM_USER')
+EMAIL_TO_USER = config('EMAIL_TO_USER')
+SERVER_EMAIL = config('EMAIL_HOST_USER')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.fog-agric.com'
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
